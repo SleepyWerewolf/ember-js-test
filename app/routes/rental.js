2d10d07b0cc8fbfc6fdec38d012d78284fb2d777
Viet Truong <viettruong2194@gmail.com>
2015-12-24 16:45:14 -0800

    Fix routing error for single rentals

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    return this.store.findRecord('rental', params.record_id);
+    model(params) {
+        return this.store.findRecord('rental', params.rental_id);
+    }
 });
