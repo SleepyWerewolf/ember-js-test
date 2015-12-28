6e9027bea038900b0e64a4b075cf71c4f73fa58d
Viet Truong <viettruong2194@gmail.com>
2015-12-27 23:09:11 -0800

    Add success/failure messages for updating

--- app/routes/rental.js
+++ app/routes/rental.js
@@ -1,30 +1,30 @@
 import Ember from 'ember';
 
-var successMessage = function() {
-    alert('Successfully updated rental!');
+var toggleSuccessMessage = function() {
+    $('.successMessage').fadeIn().delay(1000).fadeOut();
+};
+
+var toggleErrorMessage = function() {
+    $('.errorMessage').fadeIn();
 };
 
 export default Ember.Route.extend({
     model(params) {
-        return this.store.findRecord('rental', params.rental_id);
-        /*
-        var rental = this.store.findRecord('rental', params.rental_id);
-        return {
-            rental: rental,
-            successMessage: 'Successfully updated rental!',
-            errorMessage: 'Something went wrong...'
-        };
-        */
+        return this.store.findRecord('rental', params.rental_id).then(function(data) {
+            return {
+                rental: data,
+                successMessage: 'Successfully updated rental!',
+                errorMessage: 'Something went wrong...'
+            }
+        });
     },
     actions: {
         updateRental(rentalData) {
             rentalData.save()
                 .then(function(data) {
-                    alert('successfully saved');
-                    //successMessage();
+                    toggleSuccessMessage();
                 }, function(error) {
-                    alert('failed');
-                    //errorMessage();
+                    toggleErrorMessage();
                 });
         }
     }
